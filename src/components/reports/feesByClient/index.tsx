2062a5d063c116a24e6c147fe90826d1f70e2107
Andres <andresmorelos123@gmail.com>
2021-04-20 18:54:19 -0500

    Fixing Loading Fees By Client.

--- src/components/reports/feesByClient/index.tsx
+++ src/components/reports/feesByClient/index.tsx
@@ -1,64 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table, Select } from 'antd';
+import AccountantModule from '@andresmorelos/accountantmodule-sdk';
 
 const { Option } = Select;
 
-class FeesByClientsContainer extends React.Component {
-  _isMounted = false;
+interface Props {
+  API: AccountantModule;
+}
 
-  constructor(props: any) {
-    super(props);
+const FeesByClientsContainer = ({ API }: Props) => {
+  const [fees, setFees] = useState<any>([]);
+  const [clients, setClients] = useState<any>([]);
+  const [page, setPage] = useState<number>(1);
+  const [limit, setLimit] = useState<number>(100);
+  const [total, setTotal] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [currentClient, setCurrentClient] = useState<string | undefined>(
+    undefined
+  );
 
-    this.state = {
-      fees: [],
-      clients: [],
-      page: 1,
-      limit: 10,
-      total: 0,
-      loading: true,
-      currentClient: undefined,
+  // eslint-disable-next-line class-methods-use-this
+  const mapClientResults = (client: any) => {
+    return {
+      name: client.name,
+      id: client.id,
     };
+  };
 
-    this.handlePaginationChange = this.handlePaginationChange.bind(this);
-    this.onSearch = this.onSearch.bind(this);
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-    if (this._isMounted) {
-      this.getClients({});
-    }
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  onSearch(value: string) {
-    const { page, limit } = this.state;
-    this.getFees({ page: 1, limit, search: { client: value } });
-    this.setState({
-      currentClient: value,
-    });
-  }
-
-  getClients = ({ limit = 1000, search = {} }) => {
-    this.setState({
-      loading: true,
-    });
-    this.props.API.Clients()
+  const searchClients = ({ limit = 1000, search = {} }) => {
+    setLoading(true);
+    API.Clients()
       .getClients(search, { limit })
       .then((response) => {
-        this.setState({
-          clients: response.results.map(this.mapClientResults),
-        });
+        setClients(response.results.map(mapClientResults));
+        setLoading(false);
+        return null;
       })
       .catch((error) => console.error(error));
   };
 
-  getFees = ({ limit, page, search }) => {
-    this.props.API.Fees()
-      .getFees(search, { page, limit })
+  useEffect(() => {
+    searchClients({});
+  }, []);
+
+  const searchFees = ({
+    limitSize,
+    pageSize,
+    search,
+  }: {
+    limitSize: number;
+    pageSize: number;
+    search: any;
+  }) => {
+    API.Fees()
+      .getFees(search, { page: pageSize, limit: limitSize })
       // eslint-disable-next-line promise/always-return
       .then((response) => {
         const fees = response.results.map((fee) => {
@@ -77,103 +72,91 @@ class FeesByClientsContainer extends React.Component {
           };
         });
 
-        this.setState({
-          fees,
-          total: response.totalResults,
-          loading: false,
-          page,
-        });
+        setFees(fees);
+        setTotal(response.totalResults);
+        setLoading(false);
+        setPage(page);
       })
       .catch((error) => console.error(error));
   };
 
-  // eslint-disable-next-line class-methods-use-this
-  mapClientResults(client: any) {
-    return {
-      name: client.name,
-      id: client.id,
-    };
-  }
+  const onSearch = (value: string) => {
+    searchFees({ pageSize: 1, limitSize: limit, search: { client: value } });
+    setCurrentClient(value);
+  };
 
-  handlePaginationChange(page: number, pageSize: number) {
-    this.setState((state, props) => {
-      this.getFees({
-        page,
-        limit: pageSize,
-        search: {
-          client: state.currentClient,
-        },
-      });
-      return {
-        page,
-        limit: pageSize,
-      };
+  const handlePaginationChange = (pageChange: number, pageSize: number) => {
+    searchFees({
+      pageSize: pageChange,
+      limitSize: pageSize,
+      search: {
+        client: currentClient,
+      },
     });
-  }
+    setPage(pageChange);
+    setLimit(pageSize);
+  };
 
-  render() {
-    const { clients, fees, loading, total, page } = this.state;
-    const columns = [
-      {
-        title: 'Secuencia',
-        dataIndex: 'seq',
-        key: 'seq',
-      },
-      {
-        title: 'Cancelado',
-        dataIndex: 'cancelled',
-        key: 'cancelled',
-      },
-      {
-        title: 'Valor',
-        dataIndex: 'value',
-        key: 'value',
-      },
-      {
-        title: 'Cobrador',
-        dataIndex: 'user',
-        key: 'user',
-      },
-      {
-        title: 'Comisión',
-        dataIndex: 'commission',
-        key: 'commission',
-      },
-    ];
+  const columns = [
+    {
+      title: 'Secuencia',
+      dataIndex: 'seq',
+      key: 'seq',
+    },
+    {
+      title: 'Cancelado',
+      dataIndex: 'cancelled',
+      key: 'cancelled',
+    },
+    {
+      title: 'Valor',
+      dataIndex: 'value',
+      key: 'value',
+    },
+    {
+      title: 'Cobrador',
+      dataIndex: 'user',
+      key: 'user',
+    },
+    {
+      title: 'Comisión',
+      dataIndex: 'commission',
+      key: 'commission',
+    },
+  ];
 
-    return (
-      <>
-        <Select
-          showSearch
-          style={{ width: '100%' }}
-          onChange={this.onSearch}
-          placeholder="Selecione un cliente"
-          optionFilterProp="children"
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          {clients.map(({ name, id }) => (
-            <Option key={`client-${id}`} value={id}>
-              {name}
-            </Option>
-          ))}
-        </Select>
-        <Table
-          size="middle"
-          columns={columns}
-          dataSource={fees}
-          loading={loading}
-          scroll={{ y: 450 }}
-          pagination={{
-            total,
-            onChange: this.handlePaginationChange,
-            current: page,
-          }}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Select
+        showSearch
+        style={{ width: '100%' }}
+        onChange={onSearch}
+        placeholder="Selecione un cliente"
+        optionFilterProp="children"
+        filterOption={(input, option) =>
+          option?.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+      >
+        {clients.map(({ name, id }) => (
+          <Option key={`client-${id}`} value={id}>
+            {name}
+          </Option>
+        ))}
+      </Select>
+      <Table
+        size="middle"
+        columns={columns}
+        dataSource={fees}
+        loading={loading}
+        scroll={{ y: 450 }}
+        pagination={{
+          total,
+          onChange: handlePaginationChange,
+          current: page,
+        }}
+      />
+    </>
+  );
+};
 
 export default FeesByClientsContainer;
